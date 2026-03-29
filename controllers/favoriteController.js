bc877887b7acd813ceb4b7eeb24e9e011d406bab
mikimek23 <mikimek933@gmail.com>
2026-03-29 21:50:54 +0300

    feat(favorites): implement favorite controllers and update favorite routes
    - add database-backed logic for adding and removing favorites
    - protect favorite POST/DELETE endpoints with auth middleware
    - rename the route param to menuItemId and move the router into routes/

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,9 +1,54 @@
-export const getFavorites=(req,res,next)=>{
-    // code
-}
-export const addFavorite = (req, res, next)=>{
-    // code
-}
-export const removeFavorite = (req, res, next)=>{
+import { Favorite } from "../models/Favorite.js";
 
-}
+export const getFavorites = async (req, res, next) => {
+  // code
+};
+export const addFavorite = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { menuItemId } = req.params;
+    const favoriteItem = await Favorite.findOne({
+      userId: userId,
+      menuItemId: menuItemId,
+    });
+    if (favoriteItem) {
+      await Favorite.findOneAndDelete({ _id: favoriteItem._id });
+      return res.status(200).json({
+        success: true,
+        message: "Item removed from favorite",
+      });
+    }
+    const newFavorite = await Favorite.create({
+      userId: userId,
+      menuItemId: menuItemId,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Item added to favorite.",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+export const removeFavorite = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const {menuItemId} = req.params;
+    const favoriteItem = await Favorite.findOne({
+      userId: userId,
+      menuItemId: menuItemId,
+    });
+    if (!favoriteItem) {
+      const error = new Error("Item not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    await Favorite.findOneAndDelete({ _id:favoriteItem._id});
+    res.status(200).json({
+        success:true,
+        message:'Item removed from favorite'
+    })
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { addFavorite, getFavorites, removeFavorite } from '../controllers/favoriteController.js'
+import { fakeAuthMiddleware } from '../middlewares/authmiddleware.js'
 
 const favoriteRouter= express.Router()
 
 favoriteRouter.get('/',getFavorites)
-favoriteRouter.post('/:id',addFavorite),
-favoriteRouter.delete('/:id',removeFavorite)
+favoriteRouter.post('/:menuItemId',fakeAuthMiddleware,addFavorite),
+favoriteRouter.delete('/:menuItemId',fakeAuthMiddleware, removeFavorite)
 
 export default favoriteRouter
